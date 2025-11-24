e462abd094d024e58b9fa5c6760335b58b50d06a
saikumar <wwwsaikumar150@gmail.com>
2025-11-24 17:29:48 +0530

    functions

---
+++ k8-eksctl-kubectl-install.sh
@@ -0,0 +1,12 @@
+# eksctl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+mv /tmp/eksctl /usr/local/bin
+eksctl version
+VALIDATE $? "eksctl installation"
+
+
+# kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.30.0/2024-05-12/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv kubectl /usr/local/bin/kubectl
+VALIDATE $? "kubectl installation"
