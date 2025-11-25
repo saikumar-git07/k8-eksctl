d6d7a89650afeb29868bfa060516ef82137b828d
saikumar <wwwsaikumar150@gmail.com>
2025-11-25 18:41:05 +0530

    functions

--- k8-eksctl-kubectl-install.sh
+++ k8-eksctl-kubectl-install.sh
@@ -6,7 +6,7 @@ VALIDATE $? "eksctl installation"
 
 
 # kubectl
-curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.30.0/2024-05-12/bin/linux/amd64/kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.34.2/2025-11-13/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv kubectl /usr/local/bin/kubectl
 VALIDATE $? "kubectl installation"
