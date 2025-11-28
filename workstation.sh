471976beab9d8a072c246f6b4bb4ba037eeeafac
saikumar <wwwsaikumar150@gmail.com>
2025-11-28 18:06:30 +0530

    functions

--- workstation.sh
+++ workstation.sh
@@ -55,8 +55,8 @@ ln -s /opt/kubectx/kubens /usr/local/bin/kubens
 VALIDATE $? "kubens installation"
 
 
-Helm
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+#Helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-4
 chmod 700 get_helm.sh
 ./get_helm.sh
 VALIDATE $? "helm installation"
